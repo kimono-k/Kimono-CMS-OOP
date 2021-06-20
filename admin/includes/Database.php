f35c09dd1f7d8ca73d78b62635b2f98703931f58
kimono-k <kevinnarain94@gmail.com>
2021-06-20 14:08:28 +0200

    - Comments added for methods

--- admin/includes/Database.php
+++ admin/includes/Database.php
@@ -5,10 +5,17 @@ class Database
 {
     public $connection; # maybe protected, we'll see.
 
+    /**
+     * Database constructor.
+     */
     function __construct() {
         $this->open_db_connection();
     }
 
+    /**
+     * Checks if the query is executed correctly
+     * @param $result
+     */
     private function confirm_query($result) {
         if ($result) {
             die("The query is blessed by the elder gods" . $this->connection->error);
@@ -17,6 +24,9 @@ class Database
         }
     }
 
+    /**
+     * Opens the database connection
+     */
     public function open_db_connection() {
         $this->connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
         if ($this->connection->connect_errno) {
@@ -26,17 +36,28 @@ class Database
         }
     }
 
+    /** Executes a query on the database
+     * @param $sql
+     * @return mixed
+     */
     public function query($sql) {
         $result = $this->connection->query($sql);
         $this->confirm_query($result);
         return $result;
     }
 
+    /** Escapes the string to prevent hackers from doing mean stuff with our connection
+     * @param $string
+     * @return mixed
+     */
     public function escape_string($string) {
         $escaped_string = $this->connection->mysqli_real_escape_string($string);
         return $escaped_string;
     }
 
+    /** Insert the id
+     * @return mixed
+     */
     public function the_insert_id() {
         return $this->connection->insert_id;
     }
