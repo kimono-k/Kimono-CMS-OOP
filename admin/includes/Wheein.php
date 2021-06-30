a0471c6d3c8d6ac96345a69202f87fadf6e6777d
kimono-k <kevinnarain94@gmail.com>
2021-06-30 14:49:28 +0200

    - wheein gf class added - admin/test.php - autoloader class working - spl_autoload_register

---
+++ admin/includes/Wheein.php
@@ -0,0 +1,10 @@
+<?php
+# Just a class to check if autoload works
+class Wheein
+{
+    public function kissKimono() {
+        echo "<h1 style='color: white;'>Kimono, your lips are mine. 💋 x Wheein</h1>";
+        echo "<img src='https://media1.tenor.com/images/6382cfd24bf8fc123aeb1f33f4934a44/tenor.gif?itemid=5206761'>";
+    }
+}
+?>
---
+++ admin/includes/functions.php
@@ -0,0 +1,14 @@
+<?php
+# Callback function for spl_autoload_register
+function classAutoLoader($class) {
+    $class = strtolower($class);
+    $the_path = 'includes/'.$class.'.php';
+
+    if (is_file($the_path) && !class_exists($class)) {
+        require_once($the_path);
+    }
+}
+
+# Autoloader - Loads the classes needed for a specific page
+spl_autoload_register("classAutoLoader");
+?>
