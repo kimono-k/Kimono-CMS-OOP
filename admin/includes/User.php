534b2a261d6d4f3c3275065f63fc57303221a47c
kimono-k <kevinnarain94@gmail.com>
2021-07-03 14:54:12 +0200

    - second part of create method written.

--- admin/includes/User.php
+++ admin/includes/User.php
@@ -86,6 +86,13 @@ class User
        $sql .= $database->escape_string($this->password) . "', '";
        $sql .= $database->escape_string($this->first_name) . "', '";
        $sql .= $database->escape_string($this->last_name) . "')'";
+
+       if ($database->query($sql)) {
+           $this->id = $database->the_insert_id();
+           return true;
+       } else {
+           return false;
+       }
     }
 }
 ?>
