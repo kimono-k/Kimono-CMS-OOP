01e00db456031dba48ef2c4fd5c5605a623266b2
kimono-k <kevinnarain94@gmail.com>
2021-07-05 18:35:52 +0200

    - properties created in Photo.php

--- admin/includes/Photo.php
+++ admin/includes/Photo.php
@@ -1,6 +1,14 @@
 <?php
 class Photo extends DbObject
 {
+    public $photo_id;
+    public $title;
+    public $description;
+    public $filename;
+    public $type;
+    public $size;
 
+    protected static $db_table = "photos"; # change this to the db table name to make it work
+    protected static $db_table_fields = ['photo_id', 'title', 'description', 'filename', 'type', 'size'];
 }
 ?>
