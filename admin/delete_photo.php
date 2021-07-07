e05a5061d35fcc3713703ecf14cd1fc40f6bf3f4
kimono-k <kevinnarain94@gmail.com>
2021-07-07 22:59:23 +0200

    - redirect if/else created

--- admin/delete_photo.php
+++ admin/delete_photo.php
@@ -2,5 +2,15 @@
 <?php if (!$session->is_signed_in()) { redirect("login.php"); } ?>
 
 <?php
-echo "hawo";
+if (empty($_GET['photo_id'])) {
+    redirect("photos.php");
+}
+
+$photo = Photo::find_by_id($_GET['photo_id']);
+
+if ($photo) {
+    $photo->delete_photo();
+} else {
+    redirect("photos.php");
+}
 ?>
